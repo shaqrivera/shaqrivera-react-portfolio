8a4ac30ad05ff6d5efa8eb4c823d215489c89f44
Shaquille Rivera <shaq.rivera@gmail.com>
2022-03-31 16:04:43 -0400

    Added resume section

--- src/component/Resume/Resume.js
+++ src/component/Resume/Resume.js
@@ -1,5 +1,18 @@
+import PageHeader from '../PageHeader/PageHeader';
+import classes from './Resume.module.css';
+import resumeSVG from '../img/resume.svg';
+
+const resumeUrl = 'https://docs.google.com/document/d/12qly1sPTGZ7KbXJPoHDUbanoOA6cijt2o7g4qfoueBE/edit?usp=sharing';
+
 const Resume = () => {
-    return ('');
+    return (
+        <div className={classes.Resume}>
+            <PageHeader title={'My Resume'} />
+            <div className={classes.Link}>
+            <p onClick={(e) => window.open(resumeUrl, "_blank").focus}>Click here to see my resume</p>
+            </div>
+        </div>
+    );
 }
 
 export default Resume;
