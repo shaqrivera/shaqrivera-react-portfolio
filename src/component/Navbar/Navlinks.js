fc09f083f2b759561488de20762943b64b8ddd97
Shaquille Rivera <shaq.rivera@gmail.com>
2023-04-18 01:45:56 -0500

    Minor styling update

--- src/component/Navbar/Navlinks.js
+++ src/component/Navbar/Navlinks.js
@@ -4,48 +4,56 @@ const Navlinks = (props) => {
     const from = {opacity:0, y: 50}
     const to = {opacity:1, y: 0}
 
+    const scrollTo = (elementId) => {
+        window.location.hash = elementId
+        const section = document.getElementById(elementId)
+        const x = section.offsetLeft
+        const y = section.offsetTop - 80
+        window.scrollTo(x,y)
+    }
+
     return (
         <ul>
             <motion.li
             initial = {from}
             animate = {to}
              onClick={()=>props.mobile && props.closeMenu()}>
-                <a href="#home">Home</a>
+                <a onClick={() => scrollTo('home')}>Home</a>
             </motion.li>
             <motion.li
             initial = {from}
             animate = {to}
             transition = {{delay: .1}}
             onClick={()=>props.mobile && props.closeMenu()}>
-                <a href="/#about">About Me</a>
+                <a onClick={() => scrollTo('about')}>About Me</a>
             </motion.li>
             <motion.li 
             initial = {from}
             animate = {to}
             transition = {{delay: .2}}
             onClick={()=>props.mobile && props.closeMenu()}>
-                <a href="/#skills">Skills</a>
+                <a onClick={() => scrollTo('skills')}>Skills</a>
             </motion.li>
             <motion.li 
             initial = {from}
             animate = {to}
             transition = {{delay: .3}}
             onClick={()=>props.mobile && props.closeMenu()}>
-                <a href="/#work">My Work</a>
+                <a onClick={() => scrollTo('work')}>My Work</a>
             </motion.li>
             <motion.li 
             initial = {from}
             animate = {to}
             transition = {{delay: .4}}
             onClick={()=>props.mobile && props.closeMenu()}>
-                <a href="/#resume">Resume</a>
+                <a onClick={() => scrollTo('resume')}>Resume</a>
             </motion.li>
             <motion.li 
             initial = {from}
             animate = {to}
             transition = {{delay: .5}}
             onClick={()=>props.mobile && props.closeMenu()}>
-                <a href="/#contact">Contact Me</a>
+                <a onClick={() => scrollTo('contact')}>Contact Me</a>
             </motion.li>
         </ul>
     );
